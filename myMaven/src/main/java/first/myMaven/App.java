38f08cfcaab45c22b6428bfb233370f88d4ae5fd
fangwen88@yahoo.com <$gG23390917>
2019-11-29 15:04:57 -0500

    init commit

---
+++ myMaven/src/main/java/first/myMaven/App.java
@@ -0,0 +1,13 @@
+package first.myMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
